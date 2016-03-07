83dca93b7a352f07386db541ecb99cd25f77c239
Shraddha Patel <spatelrb@gmail.com>
2016-03-07 10:53:42 -0500

    assessment 5

---
+++ number.js
@@ -0,0 +1,23 @@
+// Mission:
+
+// Given an array of numbers create a javascript function that finds the largest number in that array.
+
+// DO NOT use the .max method
+
+// Requirements:
+
+// Must find the largest number given an array of numbers
+// Once largest number is found, it must be returned from the function
+
+
+// function largestNumber (n){
+	var array = [5, 12, 31, 27];
+	var largest = 0;
+
+for (i = 0, i <= largest; i++){
+	if (array[i] > largest){
+		var largest = array[i];
+	}
+}
+
+console.log(largest)
